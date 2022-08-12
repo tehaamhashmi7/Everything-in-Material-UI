bfbc115af02e44f9afa0d9615b6af958c880d59b
Tehaam Hashmi <htehaam@outlook.com>
2022-08-12 14:54:18 +0530

    Paper Component

--- src/components/12-GridComponent.tsx
+++ src/components/12-GridComponent.tsx
@@ -1,14 +1,16 @@
-import { Grid, Box } from '@mui/material'
+import { Grid, Box, Paper } from '@mui/material'
 import React from 'react'
 
 function GridComponent() {
   return (
+    <Paper sx={{padding: '32px'}} elevation={4}>
     <Grid container>
         <Grid item xs={3}><Box bgcolor='primary.light' p={2}>item-1</Box></Grid>
         <Grid item xs={9}><Box bgcolor='primary.light' p={2}>item-2</Box></Grid>
         <Grid item xs={9}><Box bgcolor='primary.light' p={2}>item-3</Box></Grid>
         <Grid item xs={3}><Box bgcolor='primary.light' p={2}>item-4</Box></Grid>
     </Grid>
+    </Paper>
   )
 }
 
