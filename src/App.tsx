3adf782cb421a450a60cd183445b1e0d5cea80fb
Tehaam Hashmi <htehaam@outlook.com>
2022-08-14 07:34:19 +0530

    Menu item navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import MuiTypography from './components/MuiTypography';
-import Buttons from './components/2-Buttons';
-import Textfield from './components/3-textfield';
-import SelectComponent from './components/4-SelectComponent';
-import RadioButton from './components/5-RadioButton';
-import Checkboxx from './components/6-checkboxx';
-import Switches from './components/7-Switches';
-import Ratings from './components/8-Ratings';
-import Autocompleted from './components/9-Autocomplete';
-import BoxComponent from './components/10-BoxComponent';
-import StackComponent from './components/11-StackComponent';
-import GridComponent from './components/12-GridComponent';
-import CardComponent from './components/13-CardComponent';
-import AccordionComponent from './components/14-AccordionComponent';
-import Imglist from './components/15-imglist';
+// import MuiTypography from './components/MuiTypography';
+// import Buttons from './components/2-Buttons';
+// import Textfield from './components/3-textfield';
+// import SelectComponent from './components/4-SelectComponent';
+// import RadioButton from './components/5-RadioButton';
+// import Checkboxx from './components/6-checkboxx';
+// import Switches from './components/7-Switches';
+// import Ratings from './components/8-Ratings';
+// import Autocompleted from './components/9-Autocomplete';
+// import BoxComponent from './components/10-BoxComponent';
+// import StackComponent from './components/11-StackComponent';
+// import GridComponent from './components/12-GridComponent';
+// import CardComponent from './components/13-CardComponent';
+// import AccordionComponent from './components/14-AccordionComponent';
+// import Imglist from './components/15-imglist';
 import NavbarComp from './components/16-NavbarComp';
 
 function App() {
--- src/components/16-NavbarComp.tsx
+++ src/components/16-NavbarComp.tsx
@@ -1,23 +1,75 @@
-import React from 'react'
-import CatchingPokemonIcon from '@mui/icons-material/CatchingPokemon'
-import { AppBar, IconButton, Toolbar, Typography, Stack, Button } from '@mui/material'
+import React, { useState } from "react";
+import CatchingPokemonIcon from "@mui/icons-material/CatchingPokemon";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import {
+  AppBar,
+  IconButton,
+  Toolbar,
+  Typography,
+  Stack,
+  Button,
+  Menu,
+  MenuItem,
+} from "@mui/material";
 
 function NavbarComp() {
+  const [anchorMenu, setMenu] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorMenu);
+
   return (
-    <AppBar position='static'>
+    <Stack direction={"row"} spacing={2}>
+      <AppBar position="static">
         <Toolbar>
-            <IconButton size='large' edge='start' color='inherit' aria-label='logo'>
-                <CatchingPokemonIcon />
-            </IconButton>
-            <Typography variant='h6' component={'div'} sx={{flexGrow: 1}}>Pokemon App</Typography>
-            <Stack direction='row' spacing={2}>
-                <Button variant='text' color='inherit'>Features</Button>
-                <Button variant='text' color='inherit'>Pricing</Button>
-                <Button variant='text' color='inherit'>About</Button>
-            </Stack>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="logo"
+          >
+            <CatchingPokemonIcon />
+          </IconButton>
+          <Typography variant="h6" component={"div"} sx={{ flexGrow: 1 }}>
+            Pokemon App
+          </Typography>
+          <Stack direction="row" spacing={2}>
+            <Button variant="text" color="inherit">
+              Features
+            </Button>
+            <Button variant="text" color="inherit">
+              Pricing
+            </Button>
+            <Button
+              id="resource-button"
+              variant="text"
+              color="inherit"
+              onClick={(e: React.MouseEvent<HTMLButtonElement>) =>
+                setMenu(e.currentTarget)
+              }
+              aria-controls={open ? "resource-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+              endIcon={
+                open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />
+              }
+            >
+              About
+            </Button>
+          </Stack>
+          <Menu
+            open={open}
+            id="resource-menu"
+            anchorEl={anchorMenu}
+            MenuListProps={{ "aria-labelledby": "resource-button" }}
+            onClose={() => setMenu(null)}
+          >
+            <MenuItem onClick={() => setMenu(null)}>Blog</MenuItem>
+            <MenuItem onClick={() => setMenu(null)}>Podcast</MenuItem>
+          </Menu>
         </Toolbar>
-    </AppBar>
-  )
+      </AppBar>
+    </Stack>
+  );
 }
 
-export default NavbarComp
+export default NavbarComp;
